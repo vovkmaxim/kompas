543f8d7c6e4c092c0138b007a07e35c121d7aa96
max <max@max-R538-R578-R778.(none)>
2014-07-28 22:53:00 +0300

    views news

--- protected/views/events/news.php
+++ protected/views/events/news.php
@@ -1,63 +1,8 @@
-<?php
-/* @var $this EventsController */
-/* @var $data Events */
-
-
-
-                print_r("<pre>");
-                print_r($model);
-                print_r("<pre>");
-                die();
-
-?>
-
 <div class="view">
-<!--
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('type')); ?>:</b>
-	<?php echo CHtml::encode($data->type); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
-	<?php echo CHtml::encode($data->title); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
-	<?php echo CHtml::encode($data->description); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('author')); ?>:</b>
-	<?php echo CHtml::encode($data->author); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('create_date')); ?>:</b>
-	<?php echo CHtml::encode($data->create_date); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('update_date')); ?>:</b>
-	<?php echo CHtml::encode($data->update_date); ?>
-	<br />-->
-
-	<?php /*
-	<b><?php echo CHtml::encode($data->getAttributeLabel('position')); ?>:</b>
-	<?php echo CHtml::encode($data->position); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('text')); ?>:</b>
-	<?php echo CHtml::encode($data->text); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('logo_title')); ?>:</b>
-	<?php echo CHtml::encode($data->logo_title); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('logo_path')); ?>:</b>
-	<?php echo CHtml::encode($data->logo_path); ?>
-	<br />
-
-	*/ ?>
-
+<?php
+$this->widget('zii.widgets.CListView', array(
+    'model'=>$model,
+    'itemView'=>'_view',
+    'template'=>"{items}\n{pager}",
+)); ?>	
 </div>
