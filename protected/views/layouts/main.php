7556efae11c85f56b1fe15ae4fa3fb846dbdf004
maxim.vovk <maxim.vovk@chdev.com.ua>
2014-08-12 14:13:14 +0300

    work pull dev

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,3 +1,110 @@
+<?php
+/* Функция генерации календаря */
+function draw_calendar($month,$year){
+  /* Начало таблицы */
+  $calendar = '<table cellpadding="0" cellspacing="0" class="calendar">';
+  /* Заглавия в таблице */
+  $headings = array('Понедельник','Вторник','Среда','Четверг','Пятница','Субота','Воскресенье');
+  $calendar.= '<tr class="calendar-row"><td class="calendar-day-head">'.implode('</td><td class="calendar-day-head">',$headings).'</td></tr>';
+  /* необходимые переменные дней и недель... */
+  $running_day = date('w',mktime(0,0,0,$month,1,$year));
+  $running_day = $running_day - 1;
+  $days_in_month = date('t',mktime(0,0,0,$month,1,$year));
+  $days_in_this_week = 1;
+  $day_counter = 0;
+  $dates_array = array();
+  /* первая строка календаря */
+  $calendar.= '<tr class="calendar-row">';
+  /* вывод пустых ячеек в сетке календаря */
+  for($x = 0; $x < $running_day; $x++):
+    $calendar.= '<td class="calendar-day-np"> </td>';
+    $days_in_this_week++;
+  endfor;
+  // ***************************************************************************
+  // ***************************************************************************
+  // ***************************************************************************
+  
+  $user = User::model()->findAll();
+  $user_dey_list = array();
+  foreach ($user as $users){
+      $mas_data = explode('-', $users->data_birth);
+      $user_dey_list[] = $mas_data[1] . '-' . $mas_data[2];
+  }
+  
+  // ***************************************************************************
+  // ***************************************************************************
+  // ***************************************************************************
+  /* дошли до чисел, будем их писать в первую строку */
+  for($list_day = 1; $list_day <= $days_in_month; $list_day++):
+    $calendar.= '<td class="calendar-day">';
+      /* Пишем номер в ячейку */
+      $data = $month . '-' . $list_day;
+      $flag = false;
+      foreach ($user_dey_list as $k => $v){
+        if($data == $v){
+            $flag = true;
+        }
+      }
+         
+      if($flag){
+          $calendar.= '<div class=""> ***'. $list_day.'***</div>';
+      } else {
+          $calendar.= '<div class="day-number">'. $list_day.'</div>';
+      }
+             
+      /** ЗДЕСЬ МОЖНО СДЕЛАТЬ MySQL ЗАПРОС К БАЗЕ ДАННЫХ! ЕСЛИ НАЙДЕНО СОВПАДЕНИЕ ДАТЫ СОБЫТИЯ С ТЕКУЩЕЙ - ВЫВОДИМ! **/
+      $calendar.= str_repeat('<p> </p>',2);
+      
+    $calendar.= '</td>';
+    if($running_day == 6):
+      $calendar.= '</tr>';
+      if(($day_counter+1) != $days_in_month):
+        $calendar.= '<tr class="calendar-row">';
+      endif;
+      $running_day = -1;
+      $days_in_this_week = 0;
+    endif;
+    $days_in_this_week++; $running_day++; $day_counter++;
+  endfor;
+  /* Выводим пустые ячейки в конце последней недели */
+  if($days_in_this_week < 8):
+    for($x = 1; $x <= (8 - $days_in_this_week); $x++):
+      $calendar.= '<td class="calendar-day-np"> </td>';
+    endfor;
+  endif;
+  /* Закрываем последнюю строку */
+  $calendar.= '</tr>';
+  /* Закрываем таблицу */
+  $calendar.= '</table>';
+  
+  /* Все сделано, возвращаем результат */
+  return $calendar;
+}
+
+function get_mont($mont){
+    $monts_array = array(
+        "01" => 'Январь',
+        "02" => 'Февраль',
+        "03" => 'Март',
+        "04" => 'Апрель',
+        "05" => 'Май',
+        "06" => 'Июнь',
+        "07" => 'Июль',
+        "08" => 'Август',
+        "09" => 'Сентябрь',
+        "10" => 'Октябрь',
+        "11" => 'Ноябрь',
+        "12" => 'Декабрь',
+    );
+    foreach ($monts_array as $k => $v){
+        if($mont == $k){
+            return $v;
+        }
+    }
+}
+
+
+?>  
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -54,13 +161,6 @@
 			<div class="top-login small-4 large-3 columns">
 				<div class="top-user">
 					<a href="#" data-dropdown="profile" data-options="is_hover:true" class="top-user-link" alt="Вход" title="Вход">Войти</a>
-					<ul id="profile" class="f-dropdown" data-dropdown-content>
-					  <li><a href="#">Личные данные</a></li>
-					  <li><a href="#">Заявки</a></li>
-					  <li><a href="#">Клубная форма</a></li>
-					  <li><a href="#">Вопросы</a></li>
-					  <li><a href="#">Выход</a></li>
-					</ul>
 				</div>
 				<div class="top-contact">
 					<a href="#" data-dropdown="calendar" data-options="is_hover:true" class="calendar"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ico-calendar.png" alt="Календарь" title="Календарь"/></a>
@@ -80,10 +180,6 @@
 <!--	<div id="header">
 		<div id="logo"><?php // echo CHtml::encode(Yii::app()->name); ?></div>
 	</div> header -->
-<<<<<<< HEAD
-
-=======
->>>>>>> 651c917416e5496042043230ad3155f47ffac27c
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
@@ -144,7 +240,17 @@
 
 	<?php echo $content; ?>
 
-	<div class="clear"></div>
+	
+	<div class="clear">
+            <?php 
+                /* КАЛЕНДАРЬ!!!!! */           
+                $user = User::model()->findAll();
+                print_r($user[0]->name);
+                $mass_data = explode('-', date('m-Y'));
+                echo '<h2>' . get_mont($mass_data[0]) . ' ' . $mass_data[1] . '</h2>';
+                echo draw_calendar($mass_data[0],$mass_data[1]);
+            ?>
+        </div>
 
 	<div id="footer">
 		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
