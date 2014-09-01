c38bca0b891bdd46aedd039cd673d6506fb80835
max <max@max-R538-R578-R778.(none)>
2014-09-01 23:30:19 +0300

    change views rank

--- protected/modules/admin/views/rank/view.php
+++ protected/modules/admin/views/rank/view.php
@@ -3,20 +3,17 @@
 /* @var $model Rank */
 
 $this->breadcrumbs=array(
-	'Ranks'=>array('index'),
+	'Разряд'=>array('index'),
 	$model->name,
 );
 
 $this->menu=array(
-//	array('label'=>'List Rank', 'url'=>array('index')),
-	array('label'=>'Create Rank', 'url'=>array('create')),
-	array('label'=>'Update Rank', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Rank', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Rank', 'url'=>array('admin')),
+	array('label'=>'Создать', 'url'=>array('create')),
+	array('label'=>'Управление', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Rank #<?php echo $model->id; ?></h1>
+<h1>Просмотр разряда</h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
