2122e0398e57ddadc65c88fb5a2b15305c6900b3
max <max@max-R538-R578-R778.(none)>
2014-09-02 01:00:59 +0300

    Change views competition

--- protected/views/competition/view.php
+++ protected/views/competition/view.php
@@ -8,14 +8,27 @@ $this->breadcrumbs=array(
 );
 
 ?>
-<div class="article">
-<h1><?php echo $model->title . ' (' . $model->start_data . ' - ' . $model->end_data . ')'     ; ?></h1>
-<p>Дата начала: <?php echo $model->start_data; ?>  - время начала: <?php echo $model->start_time; ?></p>
-<p>Дата окончания: <?php echo $model->end_data; ?>  - время окончания: <?php echo $model->end_time; ?></p>
-<?php echo $model->getLogoImage(); ?>
-<p><?php echo $model->text; ?></p>
 
-<?php
+
+<div class="large-12 columns">
+	<div id="news-item" class="large-12 small-12 columns">
+		
+		<h3>«<?php echo $model->title . ' (' . $model->start_data . ' - ' . $model->end_data . ')'     ; ?>»</h3>
+		<span class="date-time"><p>Дата начала: <?php echo $model->start_data; ?>  - время начала: <?php echo $model->start_time; ?></p></span>
+		<span class="date-time"><p>Дата окончания: <?php echo $model->end_data; ?>  - время окончания: <?php echo $model->end_time; ?></p></span>
+		<div class="img-content">
+                    <img width="563" height="291" alt="logo" src="logo_competition/<?php echo $model->logo_desc; ?>">
+                    <!--<img data-src="holder.js/100%x291/grey" alt="image">-->
+		</div>
+		<p><?php echo $model->text; ?></p>
+		<div id="mymap" class="large-12 small-12 columns"></div>
+		<div class="row">
+			<div class="large-6 small-12 columns">
+				<div class="tags">Теги: ориентирование, компас</div>
+				<div><img src="images/ico-socials.png" alt="socials"></div>
+			</div>
+			<!--<div class="large-6 small-12 columns" style="text-align: center;">-->
+                            <?php
 
 if($file != NULL){
     
@@ -47,67 +60,75 @@ if($file != NULL){
     
 }
 
-?>
-<h4>Дата создания: </h4><?php echo $model->create_date; ?></br>
-<?php
-if($model->enable_registration_flag == 1){
-    echo 'Окончание регистрации: ' . $model->close_registration_data . ' время' . $model->close_registration_data;
-    if(!Yii::app()->user->isGuest){
-        echo '<p>'.CHtml::link('Подать заявку', array('competitionRequest/application', 'id'=>$model->id)).'"</p> ';  
-    } else {
-        echo '<h6> Для подачи заявки нужно войти в систему! </h6>';  
-    }
-      
-}
+?>			<!--</div>-->
+ <span class="date-time"><p>Дата создания: </h4><?php echo $model->create_date; ?></p></span>                       
+    <?php
+        if($model->enable_registration_flag == 1){
+            echo '<span class="date-time">Окончание регистрации: ' . $model->close_registration_data . ' время' . $model->close_registration_data . '</span>';
+            if(!Yii::app()->user->isGuest){
+                echo '<p>'.CHtml::link('Подать заявку', array('competitionRequest/application', 'id'=>$model->id)).'</p> ';  
+            } else {
+                echo '<h6> Для подачи заявки нужно войти в систему! </h6>';  
+            }
 
-if($request != NULL){
- $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'competition-request-grid',
-	'dataProvider'=>$request,
-//	'filter'=>$request,
-	'columns'=>array(
-		'id',
-		array(
-                    'name' => 'group_id',
-                    'type' => 'raw',
-                    'value' => '$data->getGroupName()',
-                    'filter' => false,
-                ),
-		'name',
-		'lastname',
-		'year',		
-		'chip',
-		'dyusch',
-		'sity',
-		'coutry',
-		'team',
-		'coach',
-		'fst',
-		'participation_data',
-		array(
-                    'name' => 'Состояние',
-                    'type' => 'raw',
-                    'value' => '$data->getNameStatus()',
-                    'filter' => false,
-                ),
-                array(
-                    'name' => 'Представитель',
-                    'type' => 'raw',
-                    'value' => '$data->getNameUser()',
-                    'filter' => false,
-                ),
-		
-	),
-));
-}
+        }
+    ?>   
+		</div>
+	</div>
+</div>
+<div class="people large-12 small-12 columns">
+    <div class="people-about">Уже заявилось:</div>
+    <div>        
+        <?php        
+        if($request != NULL){
+                $this->widget('zii.widgets.grid.CGridView', array(
+                       'id'=>'competition-request-grid',
+                       'dataProvider'=>$request,
+                       'columns'=>array(
+                               'id',
+                               array(
+                                   'name' => 'group_id',
+                                   'type' => 'raw',
+                                   'value' => '$data->getGroupName()',
+                                   'filter' => false,
+                               ),
+                               'name',
+                               'lastname',
+                               'year',		
+                               'sity',
+                               'coutry',
+                               'participation_data',
+                               array(
+                                   'name' => 'Состояние',
+                                   'type' => 'raw',
+                                   'value' => '$data->getNameStatus()',
+                                   'filter' => false,
+                               ),
+                               array(
+                                   'name' => 'Представитель',
+                                   'type' => 'raw',
+                                   'value' => '$data->getNameUser()',
+                                   'filter' => false,
+                               ),
 
-
-$this->widget('zii.widgets.CListView', array(
+                       ),
+               ));
+           }        
+        ?>        
+    </div>
+</div>
+<div class="comments">
+    <?php
+    $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$comments,
 	'itemView'=>'_viewcomment',
 ));
-
-if(!Yii::app()->user->isGuest){
+    ?>
+    
+</div>
+<div id="comments-form" class="form">
+ <?php
+if(!Yii::app()->user->isGuest){ 
 ?>
         <div class="comment">
             <p>Ваш отзыв:</p>
@@ -133,7 +154,7 @@ if(!Yii::app()->user->isGuest){
                     <textarea id="Comments_text" name="Comments[text]" cols="50" rows="6"></textarea>
                 </div>
                 <div class="row buttons">
-                    <input type="submit" value="Отправить" name="yt0">
+                    <input class="large success button radius" type="submit" value="Отправить" name="yt0">
                 </div>
             </form>
         </div>
