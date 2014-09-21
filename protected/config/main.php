ba725e22801225e2478679599ace714f07fd5cd6
vovkmaxim <mvovk90@gmail.com>
2014-09-21 19:12:01 +0300

    alina PC

--- protected/config/main.php
+++ protected/config/main.php
@@ -66,7 +66,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=kompas',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		
